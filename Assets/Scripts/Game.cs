5abcb7ad6ab7c354e937f4e3d43b21535447eda2
Chineze Nwosu <chinezenwosu@microsoft.com>
2022-02-24 15:30:43 +0100

    populate question and answers in Game

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,28 +1,60 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Game : MonoBehaviour
 {
-    public Text RoundScore;
-    public Text TeamOneScore;
-    public Text TeamTwoScore;
+    private Round round;
+    public TMP_Text RoundScore;
+    public TMP_Text TeamOneScore;
+    public TMP_Text TeamTwoScore;
+    public TMP_Text Question;
+    public TMP_Text RoomId;
+    public Image AnswerContainer;
+    public GameObject AnswersPanel;
+    public Button NextQuestionButton;
+    public Button AwardTeamOneButton;
+    public Button AwardTeamTwoButton;
 
     void Start()
     {
+        round = RoundManager.Instance.CurrentRound;
+        PopulateRoomId();
         PopulateQuestion();
         PopulateAnswers();
     }
 
+    private void PopulateRoomId()
+    {
+        RoomId.text = $"ROOM {RoomManager.Instance.CurrentRoom.Id}";
+    }
+
     private void PopulateQuestion()
     {
-        foreach (Round round in RoomManager.Instance.CurrentRoom.rounds)
-        {
-            Debug.Log($"rounds ------------------------------------ { round.question}");
-        }
+        Question.text = round.Question;
     }
     
     private void PopulateAnswers()
     {
-        
+        List<Answer> answers = round.Answers;
+        foreach (Answer answer in answers)
+        {
+            Image answerItem = Instantiate(AnswerContainer);
+            GridLayoutGroup layoutGroup = AnswersPanel.GetComponent<GridLayoutGroup>();
+            TMP_Text answerText = answerItem.transform.Find("AnswerBackPlate")?.GetComponentInChildren<TMP_Text>();
+            TMP_Text pointsText = answerItem.transform.Find("PointsBackPlate")?.GetComponentInChildren<TMP_Text>();
+
+            answerText.text = answer.Text;
+            pointsText.text = answer.Points.ToString();
+            answerItem.transform.SetParent(layoutGroup.transform, false);
+        }
+
+        Destroy(AnswerContainer.gameObject);
+    }
+
+    public void AwardTeam(int team)
+    {
+
     }
 }
